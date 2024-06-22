601f5ce69512fda5ca849c5693369ebf74c8c552
MoodyEG <moodyeg2011@yahoo.com>
2024-06-22 03:02:40 +0300

    Tkinter window v0.01

---
+++ test.py
@@ -0,0 +1,11 @@
+""" Minesweeper game code """
+from tkinter import *
+from random import randint
+
+
+
+
+if __name__ == "__main__":
+	window = Tk()
+	window.title("Minesweeper")
+	window.mainloop()
